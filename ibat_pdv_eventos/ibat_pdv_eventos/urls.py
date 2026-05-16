be8ba4f97ddf494f5f56d8daea9ccfd93dfc99c1
Rafael <rafaelf@localhost>
2026-05-16 14:01:46 +0000

    Add favicon.ico to fix 404

--- ibat_pdv_eventos/ibat_pdv_eventos/urls.py
+++ ibat_pdv_eventos/ibat_pdv_eventos/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url='/static/favicon.ico', permanent=True)),
     path('admin/', admin.site.urls),
     path('', include('pdv.urls')),
 ]
